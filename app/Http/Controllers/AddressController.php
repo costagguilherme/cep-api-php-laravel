68cc32143576de0e28cfd859f13cd195b8d73650
Guilherme Costa <costa.gguilherme02@gmail.com>
2024-05-15 17:53:07 -0300

    feat: validando cep

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ShowAddressRequest;
 use App\UseCases\GetAddressByCepUseCase;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AddressController extends Controller
 {
@@ -11,6 +14,7 @@ class AddressController extends Controller
     }
     public function show(string $cep)
     {
+        Validator::validate(['cep' => $cep], ['cep' => 'required|string|regex:/^\d{8}$/']);
         $address = $this->getAddressByCepUseCase->execute($cep);
         return response()->json($address);
     }
